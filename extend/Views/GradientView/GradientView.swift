a28f0cdbede6bc1d532e0654d9e2a56bea1ef7e2
Aleksey Zgurskiy <mr.noone@icloud.com>
2019-08-09 16:50:07 +0300

    Add IBDrawable protocol

--- extend/Views/GradientView/GradientView.swift
+++ extend/Views/GradientView/GradientView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @IBDesignable
-open class GradientView: UIView {
+open class GradientView: UIView, IBDrawable {
   open var gradient: Gradient? = nil {
     didSet { setNeedsDisplay() }
   }
@@ -27,6 +27,11 @@ open class GradientView: UIView {
     self.gradient = gradient
   }
   
+  open override func prepareForInterfaceBuilder() {
+    drawForInterfaceBuilder()
+  }
+  
+  #if !TARGET_INTERFACE_BUILDER
   open override func draw(_ rect: CGRect) {
     guard
       let ctx = UIGraphicsGetCurrentContext(),
@@ -61,4 +66,5 @@ open class GradientView: UIView {
                              options: options)
     }
   }
+  #endif
 }
