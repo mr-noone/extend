8d9575ab89d4f00af38f02f0b152213541f899a7
Aleksey Zgurskiy <mr.noone@icloud.com>
2019-11-07 17:52:09 +0200

    Add image resizing

---
+++ extend/Extensions/UIKit/UIImage+Resize.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage+Resize.swift
+//  extend
+//
+//  Created by Aleksey Zgurskiy on 07.11.2019.
+//  Copyright © 2019 mr.noone. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImage {
+  @available(iOS 10.0, *)
+  func resize(new size: CGSize) -> UIImage? {
+    defer { UIGraphicsEndImageContext() }
+    UIGraphicsBeginImageContextWithOptions(size, true, 1)
+    draw(in: CGRect(origin: .zero, size: size))
+    return UIGraphicsGetImageFromCurrentImageContext()
+  }
+  
+  @available(iOS 10.0, *)
+  func resize(maxSide size: CGFloat) -> UIImage? {
+    let factor = size / max(self.size.width, self.size.height)
+    return resize(new: CGSize(width: self.size.width * factor, height: self.size.height * factor))
+  }
+}
