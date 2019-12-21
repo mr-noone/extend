8e46d0d17782dcc2fc2571e2f1e0f6f8471d4008
Aleksey Zgurskiy <mr.noone@icloud.com>
2019-12-21 18:24:20 +0200

    Fix round on UIView

--- extend/Extensions/UIKit/UIView+Round.swift
+++ extend/Extensions/UIKit/UIView+Round.swift
@@ -10,10 +10,22 @@ import UIKit
 
 public extension UIView {
   func round(corners: UIRectCorner = .allCorners, radius: CGFloat) {
-    let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-    let mask = CAShapeLayer()
-    mask.path = path.cgPath
-    self.layer.mask = mask
+    if #available(iOS 11, *) {
+      var maskedCorners = CACornerMask()
+      
+      if corners.contains(.topLeft) { maskedCorners.insert(.layerMinXMinYCorner) }
+      if corners.contains(.topRight) { maskedCorners.insert(.layerMaxXMinYCorner) }
+      if corners.contains(.bottomLeft) { maskedCorners.insert(.layerMinXMaxYCorner) }
+      if corners.contains(.bottomRight) { maskedCorners.insert(.layerMaxXMaxYCorner) }
+      
+      layer.cornerRadius = radius
+      layer.maskedCorners = maskedCorners
+    } else {
+      let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+      let mask = CAShapeLayer()
+      mask.path = path.cgPath
+      self.layer.mask = mask
+    }
   }
   
   func round(corners: UIRectCorner = .allCorners) {
